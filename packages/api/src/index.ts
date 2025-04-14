24e6f31f9826cc3987e467423a0f8836674b1295
nattoujam <Public.kyuuanago@gmail.com>
2025-04-15 00:40:08 +0900

    patch-feat: APIのログを追加

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,12 +1,14 @@
 import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
 import { serve } from "@hono/node-server";
+import { logger } from "hono/logger";
 
 import { api } from "@/api";
 
 const app = new Hono();
 
 app.use(prettyJSON());
+app.use(logger());
 app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
 
 app.route("/", api);
