74c7cf03689ee16a642177b6a570a18023251ab1
nattoujam <Public.kyuuanago@gmail.com>
2025-04-19 16:19:45 +0900

    patch-feat: cors追加

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
 import { serve } from "@hono/node-server";
 import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 
 import { api } from "@/api";
 
@@ -9,6 +10,7 @@ const app = new Hono();
 
 app.use(prettyJSON());
 app.use(logger());
+app.use("/api/*", cors());
 app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
 
 app.route("/", api);
